b7b80b2262ec06113214816d46f0c6e6fe3e57c0
Stephen Watson <stephenwatson2010@gmail.com>
2022-03-24 14:26:12 +0000

    level design

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import Game from './scenes/HelloWorldScene'
 
 const config = {
 	type: Phaser.AUTO,
 	width: 800,
-	height: 600,
+	height: 700,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 0 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [Game]
 }
 
 export default new Phaser.Game(config)
